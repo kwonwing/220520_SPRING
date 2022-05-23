9b42c133aea3b4984834312ccae33326d787db4c
kwonwing <105758802+kwonwing@users.noreply.github.com>
2022-05-23 14:59:00 +0900

    220520_SPRING

---
+++ WingSite/src/main/java/com/example/wingsite/model/Comment2Dto.java
@@ -0,0 +1,15 @@
+package com.example.wingsite.model;
+
+/**
+ * packageName : com.example.wingsite.model
+ * fileName : Comment2Dto
+ * author : naraekwon
+ * date : 2022/05/23
+ * description :
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/05/23         naraekwon          최초 생성
+ */
+public class Comment2Dto {
+}
